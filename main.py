56a90d7ce375f64ebec666e75ffcc000a954ddfd
RDMerlo <68595137+RDMerlo@users.noreply.github.com>
2022-12-16 21:51:11 +0000

    Рефакторинг кода

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import numpy as np
 import math
 
-# Функция печати в консоль матрицы
-def print_matrix(matrix, str='', before=8, after=4):
-    # Печать числа с настройкой чисел до и после точки
-    f = f'{{: {before}.{after}f}}'
-    print(str)
-    print('\n'.join([f''.join(f.format(el)
-                              for el in row)
-                     for row in matrix]) + '\n')
+def print_array(A):
+  for i in range(0, len(A), 1):
+    for j in range(0, len(A[i]), 1):
+      if (A[i][j] != None):
+        print("%.4f     " % A[i][j], end="")
+      else:
+        print("     ", "nul", end="")
+    print("")
+
+def print_vector(A):
+  for j in range(0, len(A), 1):
+    print("%.4f     " % A[j])
+  print("")
+  
+def print_vector_18(A, p=18):
+  for j in range(0, len(A), 1):
+    print(f"%.{p}f     " % A[j])
+  print("")
 
 
 # Система нелинейных уравнений
@@ -29,6 +39,14 @@ def matrix_jacobian(params):
         [2*y, 2*x, 1+2*z]
     ], float)
 
+def matrix_jacobian_for_seidel(params):
+    x, y, z = params
+    return np.array([
+        [-2*x, 2*z, 2*y],
+        [-3*z, 2*y, -3*x],
+        [-2*y, -2*x, -2*z]
+    ], float)
+
 # Метод Ньютона с точностью 1.0e-6
 def method_Newton(dot):
     eps = 1.0e-6
@@ -43,21 +61,7 @@ def method_Newton(dot):
       
       f = func(dot_new)
 
-      dot_list = dot.tolist()
-      dot1 = np.zeros(3, dtype=float)
-      k = 0
-      for row in dot_list:
-        dot1[k] = float(row[0])
-        k+=1
-
-      dot_list = dot_new.tolist()
-      dot0 = np.zeros(3, dtype=float)
-      k = 0
-      for row in dot_list:
-        dot0[k] = float(row[0])
-        k+=1
-
-      if get_norm_vector(dot0, dot1) < eps:
+      if get_norm_vector(dot, dot_new) < eps:
         break
 
       dot = dot_new
@@ -82,20 +86,24 @@ def method_seidel(x, y, z):
      x = xk_1
      y = yk_1
      z = zk_1
-    arr = np.array([[-zz1],[-zz2],[-zz3]])
+    arr = np.array([-zz1,-zz2,-zz3])
     aa= np.round(arr,decimals=16)
     return aa, k
-
 
 # Достаточное условие сходимости
 def dost_usl(dot):
   
-    dot1 = np.zeros(3)
+    # dot1 = np.array([0, 0, 0])
 
-    norm_X = np.zeros((3, 1))
-    for i in range(3):
-      norm_X[i] = (abs(dot1[i] - dot[i]))**2
-    max_sum = math.sqrt(sum(norm_X))
+    # norm_X = np.zeros((3, 1))
+    # for i in range(3):
+    #   norm_X[i] = (abs(dot1[i] - dot[i]))**2
+    # max_sum = math.sqrt(sum(norm_X))
+
+    max_sum = 0
+
+    for fi in matrix_jacobian_for_seidel(dot):
+        max_sum = max(max_sum, np.sum(abs(fi)))
 
     if (max_sum < 1):
         print("Достаточное условие сходимости выполняется\n")
@@ -113,27 +121,29 @@ def get_norm_vector(x_new, X):
     norm_X[i] = (abs(x_new[i] - X[i]))**2
   return(math.sqrt(sum(norm_X)))
 
-matrix_X0_dot_arr = [[0.1],[-0.2],[0.3]]
-
 # Точка Xo для начального приближения
+# matrix_X0_dot_arr = [0.1, -0.2,0.3]
+matrix_X0_dot_arr = [0.0, -0.2, 0.1]
 matrix_X0_dot = np.array(matrix_X0_dot_arr, float)
-# 1) Решение системы нелинейных уравнений f(x) = 0 методом простых итераций с точностью 1e-3
-arr, steps = method_seidel(matrix_X0_dot_arr[0][0], matrix_X0_dot_arr[1][0], matrix_X0_dot_arr[2][0])
-print("Метод Зейделя")
-print_matrix(arr)
-print("Количество шагов\n",steps)
-# Вектор невязки
-print("\nВектор невязки")
-print_matrix(vector_nevyazki([[0], [0], [0]], arr.copy()), "", 4, 16)
-# 2) Проверка выполнения достаточного условия сходимости
-dost_usl([0.1, -0.2, 0.3])
 
-matrix_dot_solved, steps = method_Newton(matrix_X0_dot.copy())
+arr, steps = method_seidel(matrix_X0_dot_arr[0], matrix_X0_dot_arr[1], matrix_X0_dot_arr[2])
+print("Метод Зейделя")
+print_vector_18(arr, 8)
+print("Количество шагов\n",steps)
+print("\nВектор невязки")
+r = vector_nevyazki([0, 0, 0], arr.copy())
+print_vector_18(r, 8)
+
+#Проверка выполнения достаточного условия сходимости
+dost_usl(matrix_X0_dot_arr)
+
 
 print('Метод Ньютона')
-print_matrix(matrix_dot_solved)
+matrix_dot_solved, steps = method_Newton(matrix_X0_dot.copy())
+print_vector_18(matrix_dot_solved, 8)
 print('Шагов метода:', steps)
 
 # Вектор невязки
 print("\nВектор невязки")
-print_matrix(vector_nevyazki([[0], [0], [0]], matrix_dot_solved.copy()), "", 4,16)
+r = vector_nevyazki([0, 0, 0], matrix_dot_solved.copy())
+print_vector_18(r,10)
